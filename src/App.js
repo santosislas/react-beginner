7c6d39df9ba1b2fc97d66f647b2f575ba98fc24f
DESKTOP-VLBEJNQ\alber <santosislas@gmail.com>
2023-05-30 12:00:16 -0600

    upload changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
+import './Card.css';
 
 function App() {
   return (
     <div className="App">
+      <Card/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
